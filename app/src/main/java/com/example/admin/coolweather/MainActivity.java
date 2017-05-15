bb6dc822c0ca4c61ce875eb80e77b1f7384f5077
wwbbb <648782909@qq.com>
2017-05-15 14:52:09 +0800

    不同级别返回按钮

--- app/src/main/java/com/example/admin/coolweather/MainActivity.java
+++ app/src/main/java/com/example/admin/coolweather/MainActivity.java
@@ -17,23 +17,24 @@ public class MainActivity extends AppCompatActivity {
             "山东省","山西省","陕西省", "安徽省","上海市","江苏省","福建省","广东省","海南省","四川省","云南省","贵州省","青海省",
             "甘肃省","江西省","台湾省内蒙古自治区","宁夏回族自治区","新疆维吾尔自治区","西藏自治区","广西壮族自治区","香港特别行政区",
             "澳门特别行政区"};
+
     private String[] bj={"东城区","西城区","朝阳区","丰台区","海淀区","门头沟区","房山区","通州区","顺义区","昌平区","大兴区",
             "怀柔区","平谷区","密云区","延庆区"};
-    private String[] zj={"杭州市","宁波市","温州市","嘉兴市","湖州市","绍兴市","金华市","衢州市","舟山市","台州市","丽水市"};
+
+    private String[] bjdc={"东华门街道","景山街道","交道口街道","安定门街道","北新桥街道","东四街道","朝阳门街道","建国门街道","东直门街道",
+            "和平里街道","北京站地区管理处", "王府井建设管理办公室"};
+
+    private String[] zj={"温州市","宁波市","杭州市","嘉兴市","湖州市","绍兴市","金华市","衢州市","舟山市","台州市","丽水市"};
+
+    private String[] zjwz={"鹿城区","龙湾区","瓯海区","洞头区","瑞安市","乐清市","永嘉县","平阳县","苍南县","文成县","泰顺县"};
     private String[] City={};
+    String[] city=null;
+    private int level=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button button= (Button) findViewById(R.id.button);
-        button.setVisibility(View.GONE);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
         ListView listView= (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
@@ -41,14 +42,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                String result = parent.getItemAtPosition(position).toString(); //获取选择项的值if (result=="北京市")
-                String[] city=null;
                if(result=="北京市"){
                    city=bj;
+                   level=2;
                }
-                else if(result=="浙江省"){
+                else if(result=="东城区"){
+                   city=bjdc;
+                   level=3;
+               }
+                else if (result=="浙江省"){
                    city=zj;
+                   level=2;
                }
-                else {
+               else if (result=="温州市"){
+                   city=zjwz;
+                   level=3;
+               }
+                else{
                    city=City;
                }
                 button.setVisibility(View.VISIBLE);
@@ -59,5 +69,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(level==2){
+                    city=data;
+                    level=1;
+                    button.setVisibility(View.GONE);
+                }
+                else if(level==3){
+                    if(city==bjdc)
+                        city=bj;
+                    else if (city==zjwz)
+                        city=zj;
+                    level=2;
+                }
+                ArrayAdapter<String> adapter=new ArrayAdapter<String>(
+                        MainActivity.this,android.R.layout.simple_list_item_1,city);
+                ListView listView= (ListView) findViewById(R.id.list_view);
+                listView.setAdapter(adapter);
+            }
+        });
     }
 }
